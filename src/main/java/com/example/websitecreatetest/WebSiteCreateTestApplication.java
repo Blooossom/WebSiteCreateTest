0df1dae718e486e0fc112e7a6dd4e41a4e038e87
Blooossom <taegyeong19@gmail.com>
2022-11-29 18:59:00 +0900

    Initial commit

---
+++ src/main/java/com/example/websitecreatetest/WebSiteCreateTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitecreatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteCreateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteCreateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websitecreatetest/WebSiteCreateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitecreatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteCreateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
